e588bec7bf5aa45f3435a5eb506382e015eeb2ba
Karolis Gaidukevicius <karoliz.g@gmail.com>
2021-03-12 12:38:01 +0200

    Excercise 4, extra 3 in progress (tic tac toe game history)

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -1,31 +1,11 @@
 import * as React from 'react'
 import {useLocalStorageState} from '../utils.js'
 
-function Board() {
-  const [squares, setSquares] = useLocalStorageState(
-    'squares',
-    Array(9).fill(null),
-  )
-  const nextValue = calculateNextValue(squares)
-  const winner = calculateWinner(squares)
-  const status = calculateStatus(winner, squares, nextValue)
-
-  function selectSquare(square) {
-    if (winner || squares[square] !== null) {
-      return
-    }
-    const squaresCopy = [...squares]
-    squaresCopy[square] = nextValue
-    setSquares(squaresCopy)
-  }
-
-  function restart() {
-    setSquares(Array(9).fill(null))
-  }
+function Board({onClick, squares, ...props}) {
 
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
+      <button className="square" onClick={() => onClick(i)}>
         {squares[i]}
       </button>
     )
@@ -33,7 +13,6 @@ function Board() {
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -49,18 +28,53 @@ function Board() {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
     </div>
   )
 }
 
 function Game() {
+  const [currentSquares, setCurrentSquares] = useLocalStorageState(
+    'squares',
+    Array(9).fill(null),
+  )
+  const nextValue = calculateNextValue(currentSquares)
+  const winner = calculateWinner(currentSquares)
+  const status = calculateStatus(winner, currentSquares, nextValue)
+
+  function selectSquare(square) {
+    if (winner || currentSquares[square] !== null) {
+      return
+    }
+    const squaresCopy = [...currentSquares]
+    squaresCopy[square] = nextValue
+    setCurrentSquares(squaresCopy)
+  }
+
+  function restart() {
+    setCurrentSquares(Array(9).fill(null))
+  }
+
+  function moves() {
+    return (
+      <li>
+        <button>
+          move
+        </button>
+      </li>
+    )
+  }
+  
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board onClick={selectSquare} squares={currentSquares} />
+        <button className="restart" onClick={restart}>
+          restart
+        </button>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
